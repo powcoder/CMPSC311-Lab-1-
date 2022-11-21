4e02844f35777dd28425bcf8ba1b41a28da53fb8
powcoder <powcoder@163.com>
2022-11-21 11:33:04 +0800

    CMPSC311 Lab 1

---
+++ student.c
@@ -0,0 +1,14 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+#include "student.h"
+#include <stdbool.h>
+
+
+/* Lab- 1 Due by 11th Feb 2022
+   Make sure your code looks clean
+   Write comments with your code
+   Do not foget to 'push' your code github reglarly
+   */
+   
---
+++ student.h
@@ -0,0 +1,18 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+#ifndef STUDENT_H_
+#define STUDENT_H_
+
+#include <stdbool.h>
+ int smallest(int array[], int length);
+ int sum(int array[], int length) ;
+ void swap(int *a, int *b);
+ void rotate(int *a, int *b, int *c);
+ void sort(int array[], int length);
+ void cube_primes(int array[], int length);
+ void double_armstrongs(int array[], int length) ;
+ void negate_happy(int array[], int length);
+
+#endif
